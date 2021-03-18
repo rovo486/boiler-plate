9d7bf372c01c7edd70d646d3ade4082baf573b00
rovo486 <rovo486@gmail.com>
2021-03-18 23:18:26 +0900

    auth 수정

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(bodyParser.json())
+app.use(cookieParser())
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -29,6 +32,55 @@ app.post('/register', (req, res) => {
       success: true
     })
   })
+})
+  
+app.post('/api/user/login', (req,res) => {
+  User.findOne({email : req.body.email}, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess : false,
+        message : '제공된 이메일에 해당하는 유저가 없습니다'
+      })
+    }
+    console.log('req.body.password:'+req.body.password);
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      console.log('isMatch2:'+isMatch);
+      if(!isMatch) 
+        return res.json({loginSuccess:false, message:'비밀번호가 틀렸습니다'})
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+          res.cookie('x_auth', user.token)
+          .status(200)
+          .json({loginSuccess:true, userId: user._id})
+      })
+    })
+  })
+})
+ 
+app.get('api/users/auth', auth, (req, res) => {
+
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('api/users/logout',auth, (res, req) => {
+
+  User.findOneAndUpdate({ _id: req.user._id},
+    {token: ""}
+    ,(err, user) => {
+      if(err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
 })
 
 // mongodb rovo486 , basic99
